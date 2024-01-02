1132f4c402cc79e89e0235e94b2e747875ebdc23
Saveudin <93369164+Saveudin@users.noreply.github.com>
2024-01-02 22:35:49 +0700

    Add files via upload

---
+++ 20185007-Dodi-Sore-TI-7-UAS-PWA/sw.js
@@ -0,0 +1,40 @@
+const appCacheName = `app-v1`;
+const dataCacheName = `data`;
+const staticAssets = [
+    `./`,
+    `./app.js`,
+    `./styles.css`
+];
+self.addEventListener('install', async function(){
+    const cache = await cache.open(appCacheName); 
+    cache.addAll(staticAssets);
+});
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    const url = new URL(request.url);
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(request));
+    } else {
+        event.respondWith(networkFirst(request));
+    }
+});
+
+async function cacheFirst (request) {
+    const cacheResponse = await caches.match(request);
+    return cacheResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+    const dataCache = await caches.open(dataCacheName);
+    try {
+        const networkResponse = await fetch(request);
+        dataCache.put(request, networkResponse.clone());
+        return networkResponse;
+    } catch (err){
+        const cacheResponse = await dataCache.match(`./fallback.json`);
+    }
+}
